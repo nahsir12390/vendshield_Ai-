8c08e0a3bd9e9c6c51f50b74a10b961854fa0db5
Nasiru Zakari <157424134+nahsir12390@users.noreply.github.com>
2026-06-09 21:11:52 +0100

    Force HTTPS asset root from app URL

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -20,7 +20,10 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        if ($this->app->environment('production')) {
+        $appUrl = config('app.url');
+
+        if (str_starts_with($appUrl, 'https://')) {
+            URL::forceRootUrl($appUrl);
             URL::forceScheme('https');
         }
     }
